9e0758944d9c4fd749eb4d03bf2fb8680d0b7a79
AKSHITHAREDDY-71 <akshithareddykondakindi@gmail.com>
2024-09-22 21:49:45 +0530

    X

--- planity/src/App.js
+++ planity/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Background from "./components/Background";
-import Homepagelayout from "./components/marketing-page/Homepagelayout";
-import Login from "./components/marketing-page/pages/Login";
-import Signup from "./components/marketing-page/pages/Signup";
+// import Background from "./components/Background";
+import Homepagelayout from "./components/Homepage/Homepagelayout";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+      {/* <Background /> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepagelayout />}></Route>
@@ -15,7 +16,7 @@ function App() {
           <Route path="/signup" element={<Signup />}></Route>
         </Routes>
       </BrowserRouter>
-      <Background />
+      
     </>
   );
 }
